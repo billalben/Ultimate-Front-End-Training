6ba01aa5affffc7a4218b320293f1f4f5e1e745c
Benzazoua <billalben252@gmail.com>
2023-12-04 19:19:53 +0100

    Create Full Screen Icon In Header

--- app/js/main.js
+++ app/js/main.js
@@ -24,3 +24,39 @@ dropMenus.forEach((dropMenu) => {
     }
   });
 });
+
+// ------------------------------
+/* Get the documentElement (<html>) to display the page in fullscreen */
+const openCloseFullscreen = document.querySelector(".toggle-fullscreen");
+
+openCloseFullscreen.addEventListener("click", function () {
+  this.classList.toggle("full-screen");
+
+  if (this.classList.contains("full-screen")) {
+    openFullscreen();
+  } else {
+    closeFullscreen();
+  }
+});
+
+const elem = document.documentElement;
+
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) {
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    elem.msRequestFullscreen();
+  }
+}
+
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
+}
