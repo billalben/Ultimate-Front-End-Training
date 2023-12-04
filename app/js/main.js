42738a00da272454620a17d86336e76ae1549cc3
Benzazoua <billalben252@gmail.com>
2023-12-04 15:48:32 +0100

    Create Sidebar Content Part 3

--- app/js/main.js
+++ app/js/main.js
@@ -8,3 +8,19 @@ toggleButton.addEventListener("click", () => {
   contentArea.classList.toggle("no-sidebar");
   sidebar.classList.toggle("no-sidebar");
 });
+
+// -------------------------
+const dropMenus = document.querySelectorAll(".drop-menu");
+
+dropMenus.forEach((dropMenu) => {
+  const iconToggleSubmenu = dropMenu.querySelector(".toggle-submenu");
+  const childLinks = dropMenu.parentElement.nextElementSibling;
+
+  dropMenu.addEventListener("click", () => {
+    iconToggleSubmenu.classList.toggle("down");
+
+    if (childLinks) {
+      childLinks.classList.toggle("open");
+    }
+  });
+});
